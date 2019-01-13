aa987de9a4eaf2838539f91c03a9743e8659a62b
virendra engole <virendra.engole27@gmail.com>
2019-01-13 11:18:17 +0530

    send sms

--- SimpleGit/src/com/gotu/SimpleGOTU.java
+++ SimpleGit/src/com/gotu/SimpleGOTU.java
@@ -3,12 +3,9 @@ package com.gotu;
 public class SimpleGOTU {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		SimpleGOTU s=new SimpleGOTU();
-		s.toString();
-=======
+	
+		
 		System.out.println("Hi viru");
->>>>>>> branch 'master' of https://github.com/virendraengole/demoofGit.git
+		System.out.println("hi rajesh");
 	}
 }
